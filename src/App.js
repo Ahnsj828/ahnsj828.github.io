2cd428c12a18d4cd0f4c92821c36177605ed034e
Ahnsj828 <ala828@naver.com>
2024-07-23 18:28:01 +0900

    homePage img gsap적용

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
-// import MouseEffect from "./features/MouseEffect"; // MouseEffect 컴포넌트 import
 import Home from "./pages/Home";
 import Introduce from "./pages/Introduce";
 import Project from "./pages/Project";
@@ -9,16 +8,13 @@ import Contact from "./pages/Contact";
 
 function App() {
   return (
-    <div>
-      {/* <MouseEffect />  */}
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/introduce" element={<Introduce />} />
-        <Route path="/project" element={<Project />} />
-        <Route path="/example" element={<Example />} />
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
-    </div>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/introduce" element={<Introduce />} />
+      <Route path="/project" element={<Project />} />
+      <Route path="/example" element={<Example />} />
+      <Route path="/contact" element={<Contact />} />
+    </Routes>
   );
 }
 
