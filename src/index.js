90a3b93ecfee3e256abc665ffe7564e28b68bd1b
Ahnsj828 <ala828@naver.com>
2024-07-25 12:22:18 +0900

    GlobalStyles 분리

--- src/index.js
+++ src/index.js
@@ -2,26 +2,10 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
-import { createGlobalStyle } from "styled-components";
+
 import MouseEffect from "./features/MouseEffect";
 import App from "./App";
-
-const GlobalStyles = createGlobalStyle`
- 
-  
-  * {
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
-  }
-  ul, li {
-    list-style: none;
-  }
-  a {
-    color: inherit;
-    text-decoration: none;
-  }
-`;
+import GlobalStyles from "./GlobalStyles";
 
 const root = createRoot(document.getElementById("root"));
 
