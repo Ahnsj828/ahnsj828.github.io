eb33bd76262764c047a290adc7362cb814fcd7f8
Ahnsj828 <ala828@naver.com>
2024-08-01 22:16:26 +0900

    introducePage branch 정리

---
+++ src/pages/Contact/Contanct.styled.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const ContactDiv = styled.div`
+  width: 100vh;
+  height: 100vh;
+  font-size: 500px;
+  color: red;
+`;
+
+export const ContactSection = styled.section``;
