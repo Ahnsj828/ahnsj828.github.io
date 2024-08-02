37fd48caccc7b3a336b60231519403f5fffd7a79
Ahnsj828 <ala828@naver.com>
2024-08-02 17:24:04 +0900

    img들을 배열에 담아서 props로 전달 & map함수로 코드 정리

--- src/pages/Introduce/Introduce.styled.js
+++ src/pages/Introduce/Introduce.styled.js
@@ -1,30 +1,87 @@
 import styled from "styled-components";
-export const IntroSection = styled.section`
+export const IntroContain = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-  /* width: 100%;
-  height: 100vh;
-  max-width: 80%;
-  max-height: 80vh;
-  margin: auto; */
   width: 80vw;
-  /* max-width: 80vw; */
   height: 100vh;
-  overflow-x: hidden;
-  -ms-overflow-style: none;
-  scrollbar-width: none;
-  &::-webkit-scrollbar {
-    display: none;
-  }
+`;
+
+export const IntroSection = styled.section`
+  display: flex;
+  justify-content: center;
+  align-items: start;
+  margin-top: 5%;
+  width: 85%;
+  height: 70%;
 `;
 
 export const MyPic = styled.article`
-  width: 50%;
+  width: 33%;
   height: 100%;
+  margin-right: 2%;
+  border-radius: 5%;
+  background: gray;
 `;
 
 export const AboutMe = styled.article`
-  width: 50%;
+  width: 65%;
   height: 100%;
+  display: flex;
+  border-radius: 30px;
+  overflow: hidden;
+`;
+
+export const ToolIconBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding: 5%;
+  width: 40%;
+  height: 100%;
+  background: wheat;
+`;
+
+export const ToolIconTitle = styled.p`
+  width: 90%;
+  /* height: 100%; */
+  line-height: 40px;
+  border: 1px solid #f00;
+  font-size: 2.5rem;
+  font-weight: bold;
+  text-align: center;
+  b {
+    padding-left: 0.3rem;
+    color: #ff5d2c;
+  }
+`;
+
+export const ToolIcons = styled.span`
+  width: 100%;
+  height: calc(100% - 40px);
+  border: 1px solid greenyellow;
+`;
+
+// < props로 전달하는 방법 >
+// export const ToolIcon = styled.div`
+//   width: 30px;
+//   height: 30px;
+//   background-image: url(${(props) => props.$HTML});
+//   background-position: center;
+//   background-repeat: no-repeat;
+//   background-size: cover;
+// `;
+
+export const ToolIcon = styled.div`
+  width: auto; // 너비는 자동 조절
+  height: 50px; // 높이를 50px로 고정
+  background: url(${(props) => props.$imgUrl}) center/contain no-repeat;
+  background-size: contain; // 이미지 비율에 맞춰서 조절
+`;
+
+export const ToolIntroduce = styled.div`
+  width: 60%;
+  height: 100%;
+  background: cadetblue;
 `;
