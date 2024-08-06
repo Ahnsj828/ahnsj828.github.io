23787239b01c920ca9c4f289c6358993dbececa4
Ahnsj828 <ala828@naver.com>
2024-08-06 21:15:09 +0900

    feat/aboutPage 끝

--- src/pages/About/AboutPage.js
+++ src/pages/About/AboutPage.js
@@ -1,73 +1,5 @@
-// // < map함수 활용 하는 방법 >
-// import React from "react";
-
-// // Import Swiper styles
-// import "swiper/css";
-// import "swiper/css/pagination";
-// import "swiper/css/navigation";
-
-// import "./AboutPage.css";
-// import {
-//   AboutSection,
-//   MyPic,
-//   AboutMe,
-//   ToolIconBox,
-//   ToolIconTitle,
-//   ToolIcons,
-//   ToolIcon,
-//   ToolIntroduce,
-// } from "./AboutPage.styled";
-
-// import BasicLayout from "../../layouts/BasicLayout";
-
-// const AboutPage = () => {
-//   const iconsList = [
-//     "html5",
-//     "css3",
-//     "sass",
-//     "javascript",
-//     "react",
-//     "typescript",
-//     "firebase",
-//     "git",
-//     "github",
-//     "figma",
-//     "illustrator",
-//     "photoshop",
-//   ];
-
-//   return (
-//     <BasicLayout>
-//       <AboutSection>
-//         <MyPic></MyPic>
-//         <AboutMe>
-//           <ToolIconBox>
-//             <ToolIconTitle>
-//               My Skill<b>.</b>
-//             </ToolIconTitle>
-//             <ToolIcons>
-//               {iconsList.map((iconName, idx) => {
-//                 // let imageUrl = require("../../assets/images/Section/Introduce/" +
-//                 let imageUrl = require("../../assets/images/Section/About/" +
-//                   `${iconName}-icon` +
-//                   ".png");
-//                 return <ToolIcon key={idx} $imgUrl={imageUrl} />;
-//               })}
-//             </ToolIcons>
-//           </ToolIconBox>
-//           <ToolIntroduce></ToolIntroduce>
-//         </AboutMe>
-//       </AboutSection>
-//     </BasicLayout>
-//   );
-// };
-
-// export default AboutPage;
-
-// < map함수 활용 하는 방법 >
 import React, { useState } from "react";
 
-// Import Swiper styles
 import "swiper/css";
 import "swiper/css/pagination";
 import "swiper/css/navigation";
@@ -114,26 +46,26 @@ const AboutPage = () => {
 
   const skillDescriptions = {
     html5:
-      "웹 표준을 준수하고, 구조화 할 수 있으며, 그에 따라 적절한 시맨틱 태그를 사용하여 마크업 할 수 있습니다.",
-    css3: "구조에 따라 어울리는 레이아웃으로 flex, grid 등을 사용하여 배치하고, 폰트와 색상 스타일을 적용할 수 있습니다. 미디어쿼리로 반응형 웹을 구현할 수 있습니다.",
-    sass: "Sass를 사용하여 CSS의 유지 보수를 용이하게 하고, 코드의 재사용성을 높일 수 있습니다.",
+      "웹 표준을 준수하며, 구조화된 마크업을 통해 시맨틱 태그와 새로운 기능을 활용하여 효과적으로 웹 페이지를 설계할 수 있습니다.",
+    css3: "CSS3를 활용하여 레이아웃, 색상, 폰트, 애니메이션 등 시각적 요소를 제어할 수 있습니다. Flexbox와 Grid 레이아웃으로 적절한 배치를 구현하고, 미디어 쿼리를 통해 반응형 웹 디자인을 효과적으로 구현할 수 있습니다.",
+    sass: "Sass를 활용하여 CSS의 기능을 확장하고, 변수, 중첩 규칙, 믹스인 등을 통해 코드의 재사용성을 높이며, 복잡한 스타일 시트를 효율적으로 관리할 수 있습니다. 이를 통해 유지 보수를 용이하게 하고, 스타일링 작업을 더 효과적으로 수행할 수 있습니다.",
     javascript:
-      "JavaScript를 사용하여 웹 페이지에 동적인 기능을 추가하고, 클라이언트 사이드에서 데이터 처리를 할 수 있습니다.",
+      "JavaScript를 활용하여 웹 페이지에 동적인 기능을 추가하고, 사용자 입력에 실시간으로 반응할 수 있습니다. 객체와 배열 등 자료구조를 이해하고 있으며, 외부 API 데이터를 능숙하게  활용하기 위해 꾸준히 학습하고 있습니다. 다양한 라이브러리와 프레임워크를 통해 효율적으로 작업하고 있습니다.",
     react:
-      "React를 사용하여 사용자 인터페이스를 구성하고, 컴포넌트 기반 개발로 효율적인 상태 관리를 할 수 있습니다.",
+      "React를 활용하여 사용자 인터페이스를 구축하며, 컴포넌트 기반 개발을 통해 효율적이고 재사용 가능한 UI를 작성하고 가상 DOM을 사용해 성능을 최적화합니다. 컴포넌트 기반 작업과 다양한 라이브러리 및 프레임워크 사용 경험이 있으며, 현재는 React를 더욱 능숙하게 다루기 위해 지속적으로 공부하고 있습니다.",
     typescript:
-      "TypeScript를 사용하여 정적 타입 검사를 통해 코드의 안정성을 높일 수 있습니다.",
+      "타입스크립트를 활용하여 정적 타입 지정과 코드 오류 감지, 최신 ECMAScript 기능 구현을 해보았지만, 아직 관련 지식이 부족하여 현재는 지속적으로 공부하고 있습니다.",
     firebase:
-      "Firebase를 사용하여 실시간 데이터베이스, 인증, 호스팅 등 다양한 백엔드 서비스를 손쉽게 통합할 수 있습니다.",
-    git: "Git을 사용하여 버전 관리를 수행하고, 협업과 변경 사항 추적을 효율적으로 관리할 수 있습니다.",
+      "Firebase를 활용하여 웹 애플리케이션을 지정 도메인으로 배포하고 수정한 경험이 있습니다. 현재는 Firebase의 실시간 데이터베이스, 인증 등의 추가 기능을 탐색하고 공부하고 있습니다.",
+    git: "Git을 활용하여 소스 코드의 버전을 관리하고, VS Code를 사용하여 작업물을 Git에 업로드할 수 있습니다. 다양한 브랜치와 커밋을 통해 팀과의 협업을 원활하게 진행하려고 노력하며, Git의 다양한 기능을 익히기 위해 지속적으로 탐색하고 있습니다.",
     github:
-      "GitHub를 사용하여 코드 저장소를 호스팅하고, 협업을 위한 pull request와 issue tracking 기능을 사용할 수 있습니다.",
+      "GitHub를 활용하여 코드 저장소를 호스팅하고, 작업물을 VS Code로 GitHub에 업로드하고 공유할 수 있습니다. 다양한 브랜치와 커밋을 통해 팀과의 협업을 원활하게 진행하도록 노력하고 있으며, 새로운 기능들을 찾아보면서 시도하고 있습니다.",
     figma:
-      "Figma를 사용하여 UI/UX 디자인을 작성하고, 팀원과의 실시간 협업을 통해 디자인 작업을 진행할 수 있습니다.",
+      "Figma를 활용하여 UI/UX 디자인을 작성하고, 실시간 협업을 통해 팀원과 함께 작업할 수 있습니다. 벡터 그래픽스 편집과 프로토타입 기능을 이용해 웹 페이지 레이아웃을 효과적으로 설계하며, 디자인 작업을 체계적으로 진행할 수 있습니다.",
     illustrator:
-      "Illustrator를 사용하여 벡터 그래픽을 디자인하고, 다양한 일러스트레이션 작업을 수행할 수 있습니다.",
+      "Illustrator를 사용하여 벡터 그래픽 디자인과 로고, 아이콘, 일러스트레이션 작업을 수행할 수 있으며, 정밀한 디자인 작업과 다양한 도구 활용에 능숙합니다. 또한, GTQi(그래픽기술자격 일러스트) 1급 자격증을 보유하고 있습니다.",
     photoshop:
-      "Photoshop을 사용하여 이미지 편집 및 다양한 디자인 작업을 수행할 수 있습니다.",
+      "Photoshop을 사용하여 이미지 수정, 색상 보정, 합성 등 다양한 이미지 편집 작업을 수행할 수 있습니다. GTQ(그래픽기술자격) 1급 자격증을 보유하고 있으며, 이를 통해 다양한 디자인 작업을 효과적으로 처리할 수 있습니다.",
   };
 
   const handleTabClick = (tabIndex) => {
@@ -188,7 +120,7 @@ const AboutPage = () => {
                   <SkillIcons>
                     {iconsList.map((iconName, idx) => {
                       const imageUrl = require(`../../assets/images/Section/About/${iconName}-icon.png`);
-                      const ratio = 1; // 필요에 따라 비율 조정
+                      const ratio = 1; // 비율 조정
                       return (
                         <SkillIcon
                           key={idx}
@@ -205,7 +137,7 @@ const AboutPage = () => {
                       <>
                         Hover over an icon to see details
                         <br />
-                        아이콘 위에 마우스를 올리면 세부 정보를 볼 수 있습니다
+                        아이콘 위에 마우스를 올리면 세부 정보를 볼 수 있습니다.
                       </>
                     )}
                   </SkillTxt>
--- src/pages/About/AboutPage.styled.js
+++ src/pages/About/AboutPage.styled.js
@@ -1,431 +1,3 @@
-// import styled from "styled-components";
-
-// export const AboutSection = styled.section`
-//   display: flex;
-//   justify-content: center;
-//   align-items: start;
-//   margin-top: 5%;
-//   width: 85%;
-//   height: 70%;
-// `;
-
-// export const MyPic = styled.article`
-//   width: 33%;
-//   height: 100%;
-//   margin-right: 2%;
-//   border-radius: 5%;
-//   background: gray;
-// `;
-
-// export const AboutMe = styled.article`
-//   width: 65%;
-//   height: 100%;
-//   display: flex;
-//   border-radius: 30px;
-//   overflow: hidden;
-// `;
-
-// // ToolIconBox => ToolsContain => ContentInner
-// export const ToolsContain = styled.div`
-//   display: flex;
-//   flex-direction: column;
-//   justify-content: center;
-//   align-items: center;
-//   padding: 5%;
-//   width: 40%;
-//   height: 100%;
-//   border: 1px solid #000;
-//   gap: 10%;
-// `;
-
-// export const ToolIconTitle = styled.p`
-//   width: 90%;
-//   /* height: 100%; */
-//   line-height: 40px;
-//   /* border: 1px solid #f00; */
-//   font-size: 2.5rem;
-//   font-weight: bold;
-//   text-align: center;
-//   b {
-//     padding-left: 0.3rem;
-//     color: #ff5d2c;
-//   }
-// `;
-
-// export const ToolIcons = styled.span`
-//   width: 90%;
-//   height: calc(100% - 40px);
-//   height: 100%;
-//   /* border: 1px solid greenyellow; */
-//   display: flex;
-//   flex-wrap: wrap;
-//   gap: 10%;
-//   padding-bottom: 8.65rem;
-// `;
-
-// // < props로 전달하는 방법 >
-// // export const ToolIcon = styled.div`
-// //   width: 30px;
-// //   height: 30px;
-// //   background-image: url(${(props) => props.$HTML});
-// //   background-position: center;
-// //   background-repeat: no-repeat;
-// //   background-size: cover;
-// // `;
-
-// export const ToolIcon = styled.div`
-//   display: inline-block;
-//   height: 50px;
-//   height: 3.125rem;
-//   height: 65px;
-//   height: 4.0625rem;
-//   width: 4.0625rem;
-//   background: url(${(props) => props.$imgUrl}) center/contain no-repeat;
-//   width: ${(props) => (props.$ratio ? `calc(${props.$ratio} * 50px)` : "50px")};
-
-//   /* 각 이미지에 맞는 비율 설정 */
-//   &:nth-child(1) {
-//     width: calc(5rem * 4 / 5);
-//   } /* firebase */
-//   &:nth-child(7) {
-//     width: calc(4.0625rem * 32 / 11);
-//   } /* firebase */
-//   &:nth-child(8) {
-//     width: calc(3.2rem * 24 / 10);
-//   } /* git */
-
-//   /* 마우스 올렸을 때 이미지가 커지도록 설정 */
-//   transition: transform 0.3s ease; /* 애니메이션 부드럽게 설정 */
-//   &:hover {
-//     transform: scale(1.1); /* 이미지 크기를 10% 확대 */
-//     cursor: pointer;
-//   }
-//   &:nth-child(3):hover,
-//   &:nth-child(5):hover {
-//     transform: scale(1.2); /* 이미지 크기를 10% 확대 */
-//   }
-// `;
-
-// // &:nth-child(3) {
-// //   margin-right: 3rem;
-// // }
-// // // sass
-// // &:nth-child(9) {
-// //   margin-right: 3rem;
-// // }
-// //  github
-
-// export const ToolIntroduce = styled.div`
-//   width: 60%;
-//   height: 100%;
-//   border: 1px solid #000;
-// `;
-
-// ==========================================================
-
-// import styled from "styled-components";
-
-// export const AboutSection = styled.section`
-//   display: flex;
-//   flex-direction: column;
-//   justify-content: center;
-//   align-items: start;
-//   /* margin-top: 3%; */
-//   width: 70%;
-//   height: 70%;
-// `;
-
-// // TabBtns 스타일
-// export const TabBtns = styled.nav`
-//   width: calc(100% - 1rem);
-//   margin: 0 0.5rem;
-//   overflow-x: auto;
-//   white-space: nowrap;
-// `;
-
-// export const Tab = styled.button`
-//   position: relative;
-//   display: inline-block;
-//   background: var(--background);
-//   border: 2px solid;
-//   border-width: 2px 0 0;
-//   border-radius: 6px 6px 0 0;
-//   margin-left: -35px;
-//   margin-right: 4rem;
-//   white-space: nowrap;
-//   text-transform: uppercase;
-//   font-family: "Patrick Hand";
-//   line-height: 0.8;
-//   cursor: pointer;
-
-//   &:focus span {
-//     border-radius: 0;
-//     border-bottom: 2px solid;
-//   }
-//   &:first-of-type {
-//     margin-left: 30px;
-//   }
-//   div {
-//     position: relative;
-//     padding: 6px 5px;
-//     z-index: 10;
-//     span {
-//       position: relative;
-//       display: inline-block;
-//       min-width: 6rem;
-//       border: 2px solid transparent;
-//       border-radius: 5px;
-//       padding: 6px 15px;
-//       font-size: 140%;
-//       color: #fff;
-//       z-index: 5;
-//     }
-//   }
-//   &:before,
-//   &:after {
-//     position: absolute;
-//     content: "";
-//     top: -2px;
-//     height: 100%;
-//     width: 30px;
-//     border-top: 2px solid;
-//     border-radius: 8px 8px 0 0;
-//     background: var(--background);
-//   }
-//   &:before {
-//     transform: skew(25deg);
-//     border-radius: 0 8px 0 0;
-//     border-right: 2px solid;
-//     right: -16px;
-//   }
-//   &:after {
-//     transform: skew(-25deg);
-//     border-radius: 8px 0 0 0;
-//     border-left: 2px solid;
-//     left: -16px;
-//   }
-//   &.active {
-//     position: relative;
-//     z-index: 50;
-//   }
-//   &.active span {
-//     background: white;
-//     border: 2px solid;
-//     border-radius: 5px;
-//     box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1);
-//     color: black;
-//   }
-//   &.active:focus span {
-//     outline: 1px solid rgba(255, 255, 255, 0.7);
-//   }
-//   &:nth-of-type(1) {
-//     --background: #8a2c7a; /* #640d6b보다 밝은 색상 */
-//     --background: #c85eaf;
-//   }
-//   &:nth-of-type(2) {
-//     --background: #d74b94; /* #b51b75보다 밝은 색상 */
-//     --background: #6f7fd7;
-//   }
-//   &:nth-of-type(3) {
-//     --background: #c85eaf; /* #af4d98보다 밝은 색상 */
-//   }
-//   &:nth-of-type(4) {
-//     --background: #f08eb4; /* #d66ba0보다 밝은 색상 */
-//   }
-//   &:nth-of-type(5) {
-//     --background: #f9b9b9; /* #e5a9a9보다 밝은 색상 */
-//   }
-//   &:nth-of-type(6) {
-//     --background: #85d3d5; /* #6cb6bb보다 밝은 색상 */
-//   }
-//   &:nth-of-type(7) {
-//     --background: #6f7fd7; /* #4d5daf보다 밝은 색상 */
-//   }
-// `;
-
-// // export const AboutContainer = styled.section`
-// export const AboutContents = styled.ul`
-//   position: relative;
-//   border: 2px solid;
-//   border-radius: 2px;
-//   width: 100%;
-//   height: 80%;
-//   top: -2px;
-//   left: -2px;
-//   z-index: 1;
-// `;
-
-// // export const AboutContents = styled.ul``;
-
-// export const AboutContent = styled.li`
-//   transition: all 0.3s;
-//   width: 100%;
-//   height: 100%;
-//   z-index: 1;
-//   background: var(--background);
-//   border-radius: 2px;
-//   padding: 1rem;
-//   font-family: "Yomogi", sans-serif;
-//   &:target {
-//     visibility: visible;
-//     opacity: 1;
-//   }
-//   &:nth-of-type(1) {
-//     --background: #8a2c7a; /* #640d6b보다 밝은 색상 */
-//     --background: #c85eaf;
-//   }
-//   &:nth-of-type(2) {
-//     --background: #d74b94; /* #b51b75보다 밝은 색상 */
-//     --background: #6f7fd7;
-//   }
-//   &:nth-of-type(3) {
-//     --background: #c85eaf; /* #af4d98보다 밝은 색상 */
-//   }
-//   &:nth-of-type(4) {
-//     --background: #f08eb4; /* #d66ba0보다 밝은 색상 */
-//   }
-//   &:nth-of-type(5) {
-//     --background: #f9b9b9; /* #e5a9a9보다 밝은 색상 */
-//   }
-//   &:nth-of-type(6) {
-//     --background: #85d3d5; /* #6cb6bb보다 밝은 색상 */
-//   }
-//   &:nth-of-type(7) {
-//     --background: #6f7fd7; /* #4d5daf보다 밝은 색상 */
-//   }
-// `;
-
-// export const AboutME = styled.article`
-//   display: flex;
-//   height: 100%;
-//   min-height: 20rem;
-//   border: 2px solid;
-//   background-size: 8% 8%, 2px 2px;
-//   background-color: #f9f9f9;
-//   background-image: linear-gradient(#e4e4e4 1px, transparent 1px);
-//   padding: 1.5rem;
-//   line-height: 160%;
-// `;
-
-// export const ContentInner = styled.span`
-//   padding: 20px;
-//   padding: 1.25rem;
-//   padding-right: 40px;
-//   padding-right: 2.5rem;
-//   display: flex;
-//   h4 {
-//     width: 30%;
-//     margin: 5px;
-//     margin: 0.3125rem;
-//     margin-bottom: 25px;
-//     margin-bottom: 2rem;
-//     /* text-decoration: underline; */
-//     box-shadow: inset 0 -20px 0 #bfffa1; // 형광 연두
-//     box-shadow: inset 0 -20px 0 rgba(255, 93, 44, 0.7); // 형광 연두
-//     box-shadow: inset 0 -20px 0 #ff8a66; // 형광 다홍
-//     box-shadow: inset 0 -20px 0 #ffcc00; // 노랑000
-//     box-shadow: inset 0 -20px 0 #ffb500; // 노랑2
-//     box-shadow: inset 0 -20px 0 #ff8c00; // 주황
-//     box-shadow: inset 0 -20px 0 #ffba67; // 주황2 000
-//     /* box-shadow: inset 0 -20px 0 #ff6f00; // 진한 주황 */
-//     text-transform: uppercase;
-//     font-size: 40px;
-//     font-size: 2.5em;
-//     font-weight: bold;
-//     &::before {
-//     }
-//   }
-//   p {
-//     color: #393939;
-//     font-family: "IM_Hyemin-Bold";
-//     font-size: 1.5rem;
-//     line-height: 2.5rem;
-//     letter-spacing: 0.1rem;
-//   }
-// `;
-
-// export const AboutTxt = styled.div`
-//   width: 60%;
-//   margin-right: 5%;
-//   height: 100%;
-// `;
-// export const MyPic = styled.div`
-//   width: 35%;
-//   height: 100%;
-//   background-color: gray;
-//   border-radius: 5%;
-//   overflow: hidden;
-// `;
-
-// export const MySkills = styled.article`
-//   display: flex;
-//   min-height: 20rem;
-//   border: 2px solid;
-//   background-size: 8% 8%, 2px 2px;
-//   background-color: #f9f9f9;
-//   background-image: linear-gradient(#e4e4e4 1px, transparent 1px);
-//   padding: 1.5rem;
-//   line-height: 160%;
-// `;
-
-// // export const ToolIconTitle = styled.p`
-// //   width: 90%;
-// //   line-height: 40px;
-// //   font-size: 2.5rem;
-// //   font-weight: bold;
-// //   text-align: center;
-// //   b {
-// //     padding-left: 0.3rem;
-// //     color: #ff5d2c;
-// //   }
-// // `;
-
-// export const ToolIcons = styled.span`
-//   width: 90%;
-//   height: calc(100% - 40px);
-//   height: 100%;
-//   /* border: 1px solid greenyellow; */
-//   display: flex;
-//   flex-wrap: wrap;
-//   gap: 10%;
-//   padding-bottom: 8.65rem;
-// `;
-
-// export const ToolIcon = styled.div`
-//   display: inline-block;
-//   height: 50px;
-//   height: 3.125rem;
-//   height: 65px;
-//   height: 4.0625rem;
-//   width: 4.0625rem;
-//   background: url(${(props) => props.$imgUrl}) center/contain no-repeat;
-//   width: ${(props) => (props.$ratio ? `calc(${props.$ratio} * 50px)` : "50px")};
-
-//   /* 각 이미지에 맞는 비율 설정 */
-//   &:nth-child(1) {
-//     width: calc(5rem * 4 / 5);
-//   } /* firebase */
-//   &:nth-child(7) {
-//     width: calc(4.0625rem * 32 / 11);
-//   } /* firebase */
-//   &:nth-child(8) {
-//     width: calc(3.2rem * 24 / 10);
-//   } /* git */
-
-//   /* 마우스 올렸을 때 이미지가 커지도록 설정 */
-//   transition: transform 0.3s ease; /* 애니메이션 부드럽게 설정 */
-//   &:hover {
-//     transform: scale(1.1); /* 이미지 크기를 10% 확대 */
-//     cursor: pointer;
-//   }
-//   &:nth-child(3):hover,
-//   &:nth-child(5):hover {
-//     transform: scale(1.2); /* 이미지 크기를 10% 확대 */
-//   }
-// `;
-
-// ===================================================
-
 import styled from "styled-components";
 
 export const AboutSection = styled.section`
@@ -433,12 +5,10 @@ export const AboutSection = styled.section`
   flex-direction: column;
   justify-content: center;
   align-items: start;
-  /* margin-top: 3%; */
   width: 70%;
   height: 70%;
 `;
 
-// TabBtns 스타일
 export const TabBtns = styled.nav`
   width: calc(100% - 1rem);
   margin: 0 0.5rem;
@@ -447,8 +17,8 @@ export const TabBtns = styled.nav`
 `;
 
 export const Tab = styled.button`
-  position: relative;
   display: inline-block;
+  position: relative;
   background: var(--background);
   border: 2px solid;
   border-width: 2px 0 0;
@@ -489,8 +59,8 @@ export const Tab = styled.button`
     position: absolute;
     content: "";
     top: -2px;
-    height: 100%;
     width: 30px;
+    height: 100%;
     border-top: 2px solid;
     border-radius: 8px 8px 0 0;
     background: var(--background);
@@ -527,36 +97,18 @@ export const Tab = styled.button`
   &:nth-of-type(2) {
     --background: #6f7fd7; /* #b51b75보다 밝은 색상 */
   }
-  &:nth-of-type(3) {
-    --background: #c85eaf; /* #af4d98보다 밝은 색상 */
-  }
-  &:nth-of-type(4) {
-    --background: #f08eb4; /* #d66ba0보다 밝은 색상 */
-  }
-  &:nth-of-type(5) {
-    --background: #f9b9b9; /* #e5a9a9보다 밝은 색상 */
-  }
-  &:nth-of-type(6) {
-    --background: #85d3d5; /* #6cb6bb보다 밝은 색상 */
-  }
-  &:nth-of-type(7) {
-    --background: #6f7fd7; /* #4d5daf보다 밝은 색상 */
-  }
 `;
 
-// export const AboutContainer = styled.section`
 export const AboutContents = styled.ul`
   position: relative;
-  border: 2px solid;
-  border-radius: 2px;
-  width: 100%;
-  height: 80%;
   top: -2px;
   left: -2px;
   z-index: 1;
+  border: 2px solid;
+  border-radius: 2px;
+  width: 100%;
+  height: 90%;
 `;
-
-// export const AboutContents = styled.ul``;
 
 export const AboutContent = styled.li`
   transition: all 0.3s;
@@ -572,34 +124,17 @@ export const AboutContent = styled.li`
     opacity: 1;
   }
   &:nth-of-type(1) {
-    --background: #8a2c7a; /* #640d6b보다 밝은 색상 */
     --background: #c85eaf;
   }
   &:nth-of-type(2) {
-    --background: #d74b94; /* #b51b75보다 밝은 색상 */
     --background: #6f7fd7;
-  }
-  &:nth-of-type(3) {
-    --background: #c85eaf; /* #af4d98보다 밝은 색상 */
-  }
-  &:nth-of-type(4) {
-    --background: #f08eb4; /* #d66ba0보다 밝은 색상 */
-  }
-  &:nth-of-type(5) {
-    --background: #f9b9b9; /* #e5a9a9보다 밝은 색상 */
-  }
-  &:nth-of-type(6) {
-    --background: #85d3d5; /* #6cb6bb보다 밝은 색상 */
-  }
-  &:nth-of-type(7) {
-    --background: #6f7fd7; /* #4d5daf보다 밝은 색상 */
   }
 `;
 
 export const AboutME = styled.article`
   display: flex;
-  height: 100%;
   min-height: 20rem;
+  height: 100%;
   border: 2px solid;
   background-size: 8% 8%, 2px 2px;
   background-color: #f9f9f9;
@@ -609,26 +144,20 @@ export const AboutME = styled.article`
 `;
 
 export const ContentInner = styled.span`
-  padding: 20px;
-  padding: 1.25rem;
-  padding-right: 40px;
-  padding-right: 2.5rem;
   display: flex;
+  padding: 1.25rem;
+  padding-right: 2.5rem;
   h4 {
     width: 30%;
-    margin: 5px;
     margin: 0.3125rem;
-    margin-bottom: 25px;
     margin-bottom: 2rem;
-    /* text-decoration: underline; */
-    box-shadow: inset 0 -20px 0 #bfffa1; // 형광 연두
-    box-shadow: inset 0 -20px 0 rgba(255, 93, 44, 0.7); // 형광 연두
-    box-shadow: inset 0 -20px 0 #ff8a66; // 형광 다홍
-    box-shadow: inset 0 -20px 0 #ffcc00; // 노랑000
-    box-shadow: inset 0 -20px 0 #ffb500; // 노랑2
-    box-shadow: inset 0 -20px 0 #ff8c00; // 주황
+    /* box-shadow: inset 0 -20px 0 #bfffa1; // 형광 연두 */
+    /* box-shadow: inset 0 -20px 0 rgba(255, 93, 44, 0.7); // 형광 연두 */
+    /* box-shadow: inset 0 -20px 0 #ff8a66; // 형광 다홍 */
+    /* box-shadow: inset 0 -20px 0 #ffcc00; // 노랑000 */
+    /* box-shadow: inset 0 -20px 0 #ffb500; // 노랑2 */
+    /* box-shadow: inset 0 -20px 0 #ff8c00; // 주황 */
     box-shadow: inset 0 -20px 0 #ffba67; // 주황2 000
-    /* box-shadow: inset 0 -20px 0 #ff6f00; // 진한 주황 */
     text-transform: uppercase;
     font-size: 40px;
     font-size: 2.5em;
@@ -746,10 +275,10 @@ export const SkillTxt = styled.div`
   width: 100%;
   /* border: 1px solid #f00; */
   height: 55%;
-  padding: 6%;
+  padding: 2%;
   color: #393939;
   font-family: "IM_Hyemin-Bold";
-  font-size: 1.5rem;
+  font-size: 1.3rem;
   line-height: 2.5rem;
   letter-spacing: 0.1rem;
   word-break: keep-all;
