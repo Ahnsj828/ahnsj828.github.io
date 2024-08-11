493449e4a4e06d8496133c951f86b596c479b38f
Ahnsj828 <ala828@naver.com>
2024-08-12 02:48:32 +0900

    수정

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
+import React, { useState, useEffect } from "react";
 import { RouterProvider } from "react-router-dom";
-import root from "./router/root";
+import router from "./router/router";
 import "./App.css";
+import LoadingAnimation from "./components/LoadingAnimation";
 
 function App() {
-  return <RouterProvider router={root} />;
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 1500); // 3초
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  if (loading) {
+    return <LoadingAnimation />;
+  }
+
+  return <RouterProvider router={router} />;
 }
 
 export default App;
