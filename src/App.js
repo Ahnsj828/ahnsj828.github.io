0d2ce167af5dea512886580ccd5d0784cbe5e9b7
Ahnsj828 <ala828@naver.com>
2024-08-12 00:43:09 +0900

    contactPage done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import { RouterProvider } from "react-router-dom";
-import root from "./router/root";
+import router from "./router/router"; // root.js에서 라우터를 가져옵니다.
 import "./App.css";
 
 function App() {
-  return <RouterProvider router={root} />;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
