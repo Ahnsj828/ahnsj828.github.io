5ef6dc299c5f0449efefdfaa85d8c224ccd27a95
Ahnsj828 <ala828@naver.com>
2024-08-11 15:28:49 +0900

    contact page

--- src/pages/Contact/ContactPage.js
+++ src/pages/Contact/ContactPage.js
@@ -1,5 +1,16 @@
 import React from "react";
 
+import contactBg from "../../assets/images/Section/Contact/contactBg.png";
+import contactC from "../../assets/images/Section/Contact/contactC.png";
+import contactG from "../../assets/images/Section/Contact/contactG.png";
+import {
+  ContactSection,
+  ContactnBg,
+  ContactImg,
+  ContactTitles,
+  ContactTitle,
+} from "./ContactPage.styled";
+
 import { ContactSection } from "./ContactPage.styled";
 
 import BasicLayout from "../../layouts/BasicLayout";
